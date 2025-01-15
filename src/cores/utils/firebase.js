fea855e7f5fc83d0187078d3e0d2d613ddeb75e6
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2025-01-16 02:23:58 +0700

    setup firebase

--- src/cores/utils/firebase.js
+++ src/cores/utils/firebase.js
@@ -4,12 +4,12 @@ import {getDatabase} from "firebase/database";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "sarah-test-2f539.firebaseapp.com",
-    databaseURL: "https://sarah-test-2f539-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "sarah-test-2f539",
-    storageBucket: "sarah-test-2f539.appspot.com",
-    messagingSenderId: "449948329852",
-    appId: "1:449948329852:web:c6e55ebd62c5bec1bce6f9",
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
